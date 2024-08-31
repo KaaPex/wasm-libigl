3295f19a701db3d3d6982d47c237e3ae27cddd8a
Aleksei Kazantsev <develop.kaa@gmail.com>
2024-08-31 21:30:06 +0300

    add build config

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-echo "Generating makefile..."
+# echo "Generating makefile..."
 rm -rf build
 mkdir build && cd build
 
-emcmake cmake -DCMAKE_BUILD_TYPE=Release -DLIBIGL_USE_STATIC_LIBRARY=ON ../libigl
-emmake make -j
+emcmake cmake -DCMAKE_BUILD_TYPE=Release ..
+emmake make
---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#define CGAL_EIGEN3_ENABLED
+
+#include <iostream>
+#include <igl/copyleft/cgal/mesh_boolean.h>
+#include <Eigen/Dense>
+#include <emscripten/bind.h>
+
+// Function to perform a boolean operation on two meshes
+void mesh_boolean(
+    const Eigen::MatrixXd &V1,
+    const Eigen::MatrixXi &F1,
+    const Eigen::MatrixXd &V2,
+    const Eigen::MatrixXi &F2,
+    const std::string &operation,
+    MatrixXd &Vout,
+    MatrixXi &Fout)
+{
+    igl::MeshBooleanType boolean_type;
+    if (operation == "union")
+        boolean_type = igl::MESH_BOOLEAN_TYPE_UNION;
+    else if (operation == "intersection")
+        boolean_type = igl::MESH_BOOLEAN_TYPE_INTERSECT;
+    else if (operation == "difference")
+        boolean_type = igl::MESH_BOOLEAN_TYPE_MINUS;
+    else if (operation == "xor")
+        boolean_type = igl::MESH_BOOLEAN_TYPE_XOR;
+    else if (operation == "resolve")
+        boolean_type = igl::MESH_BOOLEAN_TYPE_RESOLVE;
+
+    igl::copyleft::cgal::mesh_boolean(V1, F1, V2, F2, boolean_type, Vout, Fout);
+}
+
+// Binding code
+EMSCRIPTEN_BINDINGS(mesh_module)
+{
+    emscripten::function("mesh_boolean", &mesh_boolean);
+    emscripten::register_vector<double>("VectorDouble");
+    emscripten::register_vector<int>("VectorInt");
+}
